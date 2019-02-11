e7b9c5735f8979fc2499302b5ef25a26717f9dd7
Nicolas Derumigny <nderumigny@gmail.com>
2019-02-10 20:40:25 -0700

    Initial commit
    Using GCC 8.2 and SVN repository.

---
+++ bench.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$1" == "" ]; then
+	NB_THREADS=$((`lscpu | grep "CPU(s):" | sed -r s/.*\([0-9]+\).*/\\\1/g` + 1))
+else
+	NB_THREADS=$1
+fi
+svn co svn://gcc.gnu.org/svn/gcc/tags/gcc_8_2_0_release gcc
+cd gcc
+mkdir build
+cd build
+../configure --target=x86_64-linux-gnu --disable-multilib
+time make -j$NB_THREADS
