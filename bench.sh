895b668ca105b1c2a09904984f5c45182ebd3539
Nicolas Derumigny <nderumigny@gmail.com>
2019-05-30 23:18:43 +0200

    Updating core detection

--- bench.sh
+++ bench.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" == "" ]; then
-	NB_THREADS=$((`lscpu | grep "CPU(s):" | sed -r s/.*\([0-9]+\).*/\\\1/g` + 1))
+	NB_THREADS=$((1+`lscpu | grep "^CPU(s):" | sed -E "s/.*([0-9]+).*/\1/g"`))
 else
 	NB_THREADS=$1
 fi
