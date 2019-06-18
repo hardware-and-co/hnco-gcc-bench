0699f3e6d4286f8d87de62e6917aa3f0b3ba865a
Nicolas Derumigny <nd@comptoir-hardware.com>
2019-06-18 19:18:48 +0200

    Can be executed directly from any location

--- bench.sh
+++ bench.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+set -e
+DIR=`dirname $0`
 if [ "$1" == "" ]; then
-	NB_THREADS=$((1+`lscpu | grep "^CPU(s):" | sed -E "s/.* ([0-9]+)/\1/g"`))
+	NB_THREADS=$((`lscpu | grep "^CPU(s):" | sed -E "s/.* ([0-9]+)/\1/g"`))
 else
 	NB_THREADS=$1
 fi
+cd $DIR
 if [ ! -d gcc ]; then
 	svn co svn://gcc.gnu.org/svn/gcc/tags/gcc_8_2_0_release gcc
 fi
 cd gcc
-mkdir build 2>&1 >/dev/null
+if [ ! -d build ]; then
+  mkdir build
+fi
 cd build
 rm -rf *
 ../configure --target=x86_64-linux-gnu --disable-multilib
