bef7c3ae97007ff56d798a943b5cdd3bf64cacbb
Nicolas Derumigny <nderumigny@gmail.com>
2024-10-06 09:45:35 +0200

    format(bench): minor formating

--- bench.sh
+++ bench.sh
@@ -5,19 +5,19 @@ DIR=`dirname $SELF`
 if [ "$1" == "" ]; then
     NB_THREADS=$((`lscpu | grep "^Processeur(s)" | sed -E "s/.* ([0-9]+)/\1/g"`))
 else
-	NB_THREADS=$1
+    NB_THREADS=$1
 fi
 cd $DIR
 if [ ! -d gcc ]; then
-	git clone --branch releases/gcc-11.2.0 --depth 1 git://gcc.gnu.org/git/gcc.git
+    git clone --branch releases/gcc-11.2.0 --depth 1 git://gcc.gnu.org/git/gcc.git
 fi
 cd -
 cd $DIR/gcc
 if [ ! -d build ]; then
-  mkdir build
+    mkdir build
 else
-  rm -rf build
-  mkdir build
+    rm -rf build
+    mkdir build
 fi
 cd -
 cd $DIR/gcc/build
